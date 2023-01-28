9ac91daab60b894c75d96be531a7ec7bdd8faef9
Stanislav Davydenko <stanislav.davydenko@sysgears.com>
2023-01-28 19:01:59 +0200

    function for env validation used

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { validateEnv } from './validateEnv';
 async function bootstrap() {
+  validateEnv();
   const app = await NestFactory.create(AppModule, { cors: true });
   const PORT = app.get(ConfigService).get<number>('PORT') ?? 3000;
   await app.listen(PORT);
