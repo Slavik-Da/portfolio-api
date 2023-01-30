4434f7e5eb842d737c58636a5ad587a56c3958ce
Stanislav Davydenko <stanislav.davydenko@sysgears.com>
2023-01-30 13:18:44 +0200

    read some vars for database.module.ts from env file

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -13,9 +13,15 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         port: configService.get<number>('DB_PORT'),
         username: configService.get<string>('DB_USER'),
         password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
+        database: configService.get<string>('DB_NAME'),
         autoLoadEntities: true,
-        synchronize: configService.get<boolean>('IS_DB_SYNCHRONIZED'),
+        synchronize: configService.get<boolean>('DB_IS_SYNCHRONIZED'),
+        logging: configService.get<boolean>('DB_IS_LOGGING'),
+        migrations: configService.get<string[]>('DB_MIGRATIONS'),
+        migrationsRun: configService.get<boolean>('DB_MIGRATIONS_RUN'),
+        migrationsTableName: configService.get<string>(
+          'DB_MIGRATIONS_TABLE_NAME'
+        ),
       }),
     }),
   ],
