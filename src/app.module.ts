40761e9bdc6f3dffc2f547d9ad90ca6a47cb5c61
Stanislav Davydenko <stanislav.davydenko@sysgears.com>
2023-01-31 18:25:11 +0200

    new db module used instead of older one

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
+import { DataSource } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseOptions } from './database/database';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    DatabaseModule,
+    TypeOrmModule.forRoot(databaseOptions),
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly dataSource: DataSource) {}
+}
