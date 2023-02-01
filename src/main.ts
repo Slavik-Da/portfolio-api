ceff712733632057d4d730a0d80cb51d667369fa
Stanislav Davydenko <stanislav.davydenko@sysgears.com>
2023-02-01 20:06:49 +0200

    validation for requests added

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { validateEnv } from './validateEnv';
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   validateEnv();
   const app = await NestFactory.create(AppModule, { cors: true });
   const PORT = app.get(ConfigService).get<number>('PORT') ?? 3000;
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(PORT);
 }
 bootstrap();
