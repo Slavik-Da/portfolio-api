52abf159b1ec5722e0b73c79b5eba89a9b9bb55d
Stanislav Davydenko <stanislav.davydenko@sysgears.com>
2023-02-01 20:37:44 +0200

    refresh-token decorator created

---
+++ src/auth/decorators/refresh-token.decorator.ts
@@ -0,0 +1,8 @@
+import { createParamDecorator, type ExecutionContext } from '@nestjs/common';
+
+export const RefreshTokenFromHeaders = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const req = ctx.switchToHttp().getRequest();
+    return req.headers['refresh-token'];
+  }
+);
