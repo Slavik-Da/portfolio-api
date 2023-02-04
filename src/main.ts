7844e34d334b4182fc9a461c4c1796ec6ff928a2
Stanislav Davydenko <stanislav.davydenko@sysgears.com>
2023-02-04 13:40:17 +0200

    global interceptor connected(for excluding some private db fields)

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { validateEnv } from './validateEnv';
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   validateEnv();
   const app = await NestFactory.create(AppModule, { cors: true });
   const PORT = app.get(ConfigService).get<number>('PORT') ?? 3000;
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   await app.listen(PORT);
 }
