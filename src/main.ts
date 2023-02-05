79d64da867cf98d2e53e5db560d392fbb1a7bc4e
Stanislav Davydenko <stanislav.davydenko@sysgears.com>
2023-02-05 20:55:28 +0200

    httpExceptionFilter used

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,15 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { validateEnv } from './validateEnv';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/libs/http-exception.filter';
 async function bootstrap() {
   validateEnv();
   const app = await NestFactory.create(AppModule, { cors: true });
   const PORT = app.get(ConfigService).get<number>('PORT') ?? 3000;
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(PORT);
 }
 bootstrap();
